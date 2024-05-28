dd1c0844888c95d23624731013647a18a9c5500e
Turi Balázs <developer@bali0531.hu>
2024-05-28 09:00:08 +0200

    Update DiscordWebook.java

--- src/main/java/org/bali/boxpvp/DiscordWebook.java
+++ src/main/java/org/bali/boxpvp/DiscordWebook.java
@@ -145,7 +145,7 @@ public class DiscordWebhook {
         connection.setRequestMethod("POST");
 
         OutputStream stream = connection.getOutputStream();
-        stream.write(json.toString().getBytes());
+        stream.write(json.toString().getBytes("UTF-8"));
         stream.flush();
         stream.close();
 
